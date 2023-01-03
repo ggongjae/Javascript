d3ec2a9aca85ec0a43072dcf14d2a91f9dd3fde1
ggongjae <cjdak0907@naver.com>
2023-01-03 10:34:30 +0900

    yongjae

---
+++ app.js
@@ -0,0 +1,11 @@
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfweek = mon + tue + wed + thu + fri + sat + sun;
+
+console.log(daysOfweek);
